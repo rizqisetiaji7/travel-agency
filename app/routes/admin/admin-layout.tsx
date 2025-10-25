37d052eb621b79a5627dd466da90f12f526b8ee9
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-10-25 11:44:11 +0700

    Add server-side clientLoader for admin auth and redirects

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,28 @@
 import { Outlet } from "react-router";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import { MobileSidebar, NavItems } from "components";
+import { account } from "~/appwrite/client";
+import { redirect } from "react-router";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
+
+export async function clientLoader() {
+    try {
+        const user = await account.get();
+
+        if (!user.$id) return redirect("/sign-in");
+
+        const existingUser = await getExistingUser(user.$id);
+
+        if (existingUser?.status === "user") {
+            return redirect("/");
+        }
+
+        return existingUser?.$id ? existingUser : await storeUserData();
+    } catch (error) {
+        console.log("Error in clientLoader:", error);
+        return redirect("/sign-in");
+    }
+}
 
 const AdminLayout = () => {
     return (
