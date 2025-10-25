211b60cd6f623c58b5c9e3581e52f0673d6b81e3
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-10-25 16:17:24 +0700

    Add sentry setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import {
+    sentryReactRouter,
+    type SentryReactRouterBuildOptions,
+} from "@sentry/react-router";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    ssr: {
-        noExternal: [/@syncfusion/],
-    },
+const sentryConfig: SentryReactRouterBuildOptions = {
+    org: "rizqi-setiaji",
+    project: "javascript-react",
+    // An auth token is required for uploading source maps;
+    // store it in an environment variable to keep it secure.
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+};
+
+export default defineConfig((config) => {
+    return {
+        plugins: [
+            tailwindcss(),
+            tsconfigPaths(),
+            reactRouter(),
+            sentryReactRouter(sentryConfig, config),
+        ],
+        ssr: {
+            noExternal: [/@syncfusion/],
+        },
+    };
 });
